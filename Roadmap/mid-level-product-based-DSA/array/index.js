6efafd62463918dba12bb51f4b59ffcdf6271a55
Vicky Gupta <vicky.k.g@thecodebucket.com>
2026-03-19 17:24:21 +0530

    two sum added with brute force solution

--- Roadmap/mid-level-product-based-DSA/array/index.js
+++ Roadmap/mid-level-product-based-DSA/array/index.js
@@ -1,1 +1,24 @@
 // Given an array of integers, find the two numbers that add up to a target sum. Return their indices.
+
+function twoSum(arr, targetSum){
+    let allSums = {}
+    let bruteSum = 0;
+    //steps - first check if array is empty or not  a valid array then return valid array
+    if(arr.length === 0 || !Array.isArray(arr) ){
+        return console.log("Enter a valid array")
+    }
+    // if array length is grater than 0 then find the targeted sum
+    else{
+     // brute force 
+     for(let i= 0; i<arr.length-1; i++){
+        for(j=i+1; j<arr.length; j++){
+            if(arr[i]+arr[j] === targetSum){
+                return console.log({number1 : arr[i], index1 : i, number2 : arr[j], index2 : j})
+            }
+        }
+     }
+     return console.log("No Pair for with the targeted sum in this array")
+    }
+}
+
+twoSum([2, 7, 11, 15],19)
